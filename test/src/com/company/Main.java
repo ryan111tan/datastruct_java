d1c21965e959fc985d9529521b9847611efd7ce5
Ryan Tan <ryan111tan@berkeley.edu>
2021-01-05 19:05:45 -0800

    Hello world and intellj setup

---
+++ test/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is the first code I written in a while :D");
+        System.out.println("Hello World btw.");
+    }
+}
